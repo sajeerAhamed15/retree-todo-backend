46b16d43cf708a085c67db2b39fdb7e3c88c9153
Shehana Iqbal <iqbalshehana@gmail.com>
2020-07-09 12:26:18 +0530

    --- Auto-Git Commit ---

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,9 +1,7 @@
 module.exports = {
-  HOST: "us-cdbr-iron-east-04.cleardb.net",
+  HOST: "localhost",
   USER: "root",
-  PASSWORD: "46628fb3",
-  DB: "heroku_03e7f2d9e42dd22"
+  PASSWORD: "",
+  DB: "retree-toDO"
 };
 
-
-// todo: Hi dumb person, change things in this 
